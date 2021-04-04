88c61d41e40e2e30b159faf90ab24b2e4f691c1c
Carl <tailcalled@gmail.com>
2021-04-04 16:55:32 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+git commit -a
+cd gulp
+yarn gulp main.build.prod
+cd ..
 rsync -avz build/* root@personality.surveydata.online:/var/www/personality.surveydata.online/orderliness/shapez.io
 #ssh root@personality.surveydata.online command
 rsync -avz server/*.py root@personality.surveydata.online:/var/www/personality.surveydata.online/orderliness/
